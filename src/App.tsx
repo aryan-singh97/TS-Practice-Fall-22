4e33d89b962e70a04211c68a17dabd1f6003da1f
Aryan Singh <aryan17aug@gmail.com>
2022-02-13 18:34:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import UseContextComponent from "./TypeScript Cheats/UseContextComponent";
+import UseEffectComponent from "./UseEffectComponent";
+import UseStateComponent from "./UseStateComponents";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
