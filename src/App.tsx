5e77da7d900bf35fae77dce3bfe9433513fb9abf
Aryan Singh <aryan17aug@gmail.com>
2022-02-14 10:52:04 +0530

    Todo state hooks, handle onSubmit event

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import "./App.css";
+import InputField from "./Components/InputField";
+import { Todo } from "./Components/model";
 import UseContextComponent from "./TypeScript Cheats/UseContextComponent";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+  return (
+    <div className="App">
+      <h1 className="heading">Taskify</h1>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Components/InputField.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Component } from "react";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  return (
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter some text"
+        className="input_box"
+      />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
