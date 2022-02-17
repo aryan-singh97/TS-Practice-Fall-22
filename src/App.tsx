ac4d842266eb317d7e52ec1ce34a651c4026411c
Aryan Singh <aryan17aug@gmail.com>
2022-02-17 14:07:36 +0530

    drag and drop implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { idText } from "typescript";
 import "./App.css";
 import InputField from "./Components/InputField";
 import { Todo } from "./Components/model";
@@ -7,6 +9,7 @@ import TodoList from "./Components/TodoList";
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -17,16 +20,62 @@ const App: React.FC = () => {
     }
   };
 
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+
+    console.log(result);
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    let add;
+    let active = todos;
+    let complete = completedTodos;
+    // Source Logic
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    // Destination Logic
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+
   console.log(todos);
   return (
-    <div className="App">
-      <h1 className="heading">Taskify</h1>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      {/* {todos.map((t) => (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <h1 className="heading">Taskify</h1>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        {/* {todos.map((t) => (
         <li>{t.todo}</li>
       ))} */}
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 };
 
